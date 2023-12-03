a114d05ac4c08c7aa3bdba443b9895c3b147dad8
af8a2a <1125209282@qq.com>
2023-12-03 16:54:48 +0800

    user login

---
+++ src/main/java/com/example/work/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.work.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.work.entity.User;
+import com.example.work.mapper.UserMapper;
+import com.example.work.service.IUserService;
+import jakarta.annotation.Resource;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Resource
+    private UserMapper mapper;
+    @Override
+    public String Login(User user) {
+        User selected=mapper.selectById(user.getUserId());
+        if(selected!=null&&selected.getPassword().equals(user.getPassword())){
+            return "success";
+        }
+        return "error";
+    }
+
+    @Override
+    public String Register(User user) {
+        return null;
+    }
+}
